6cd45b5a8c2097ff5469bcae8ac5e49a11a661fd
Saptorshe Das <iamsaptorshe07@gmail.com>
2021-02-08 19:11:59 +0530

    2nd push

--- accounts/models.py
+++ accounts/models.py
@@ -27,7 +27,7 @@ class MyAccountManager(BaseUserManager):
 
 class User(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
